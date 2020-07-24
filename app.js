db153a59e593326bb9e169e19bcfff345fc3f774
Nayan singhai <nayansinghai66@gmail.com>
2020-07-24 22:48:48 +0530

    updated

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const nodemailer = require('nodemailer');
-async function main(){
+async function main(email,output){
 let transporter = nodemailer.createTransport({
     service:'gmail',
     auth:{
@@ -10,18 +10,18 @@ let transporter = nodemailer.createTransport({
 });
 
 // send mail with defined transport object
-let info = await transporter.sendMail({
+return  transporter.sendMail({
     from: "nayansinghai27@gmail.com",
-    to: "nayansinghai66@gmail.com", // list of receivers
+    to: email+", nayansinghai66@gmail.com", // list of receivers
     subject: "Hello ✔", // Subject line
     text: "Hello world?", // plain text body
-    html: "<b>Hello world?</b>", // html body
+    html: output, // html body
   });
 
-  console.log("Message sent: %s", info.messageId);
+  //console.log("Message sent: %s", info.messageId);
   // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
  
 }
 
-main().catch(console.error);
+module.exports = main;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
+const nodemailerapp = require('./app');
 
 const app = express();
 const port = 3000;
@@ -12,40 +13,22 @@ res.sendFile(__dirname + '/index.html')}
 );
 
 app.post('/mail', function (req, res) {
+    console.log(req.body);
     let output = `
     <h1>name: ${req.body.name}</h1>
     <h2>email: ${req.body.email}</h2>
     `;
+    nodemailerapp(req.body.email,output).then(info=>{
+        console.log("email sent");
+        res.send("Email sent");
+        console.log(info);
+    }).catch((err,info)=>{
+        console.log(info);
+        console.log(err);
+        res.send("invalid email address");
+    })
     
-async function main(){
-let transporter = nodemailer.createTransport({
-    service:'gmail',
-    auth:{
-        user:'nayansinghai27@gmail.com',
-        pass:'Topperdrish@gmail98'
-    }
     
-});
-
-// send mail with defined transport object
-let info = await transporter.sendMail({
-    from: "nayansinghai27@gmail.com",
-    to: req.body.email, // list of receivers
-    subject: "Test Mail", // Subject line
-    text: "Hello world?", // plain text body
-    html: output, // html body
-  });
-
-  console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
- 
-}
-
-main().catch(console.error);
-
-    console.log(req.body);
-    res.send("Email sent");
    
   })
 
